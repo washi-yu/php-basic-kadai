264c361b68e3c0b6bee0df8772f322c61b9c2ba3
washi-yu <151375073+washi-yu@users.noreply.github.com>
2023-12-15 21:49:22 +0900

    Add files via upload

---
+++ score.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_005</title>
+</head>
+
+<?php
+  
+$score_total = 80+60+55+40+100+25+80+95+30+60;
+
+
+echo $score_total/10;
+
+
+
+?>
+</html>
