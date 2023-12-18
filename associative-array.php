762474dba35ebe3a27bd19135dd2648d34cb5fe3
washi-yu <151375073+washi-yu@users.noreply.github.com>
2023-12-18 21:46:55 +0900

    Add files via upload

---
+++ associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang=ja>
+    <head>
+        <meta charset="UTF-8">
+        <title>kadai_007</title>
+    </head>
+<body>
+    <?php
+    $onion = ['name'=>'onion','price'=>200,'weight'=>160];
+
+    print_r($onion);
+    ?>
+    
+</body>
+
+</html>
